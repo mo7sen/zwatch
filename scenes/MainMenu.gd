d16ee33e21fcbfb63ce003d2909c10f0ce604344
Robear Selwans <robear.selwans@outlook.com>
2021-01-18 22:01:26 +0200

    Sped up scene-switch time
    Signed-off-by: Robear Selwans <robear.selwans@outlook.com>

--- scenes/MainMenu.gd
+++ scenes/MainMenu.gd
@@ -1,7 +1,12 @@
 extends Control
 
+var gameScene = load("res://scenes/World.tscn").instance()
+
 func _on_NewGame_Button_pressed():
-	var _a = get_tree().change_scene("res://scenes/World.tscn")
+	var level = get_tree().root.get_node("Control")
+	get_tree().root.add_child(gameScene)
+	get_tree().root.remove_child(level)
+	level.call_deferred("free")
 
 
 func _on_QuitGame_Butoon_pressed():
