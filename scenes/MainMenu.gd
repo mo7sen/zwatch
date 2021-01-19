b76bef00a43fca63b8675ed815395fad9610c165
Robear Selwans <robear.selwans@outlook.com>
2021-01-19 16:45:32 +0200

    Merged local changes to remote
    Signed-off-by: Robear Selwans <robear.selwans@outlook.com>

--- scenes/MainMenu.gd
+++ scenes/MainMenu.gd
@@ -1,9 +1,12 @@
 extends Control
 
-onready var gameScene = preload("res://scenes/World.tscn")
+onready var gameScene# = preload("res://scenes/World.tscn")
 
-func _on_NewGame_Button_pressed():
-	var _a = get_tree().change_scene_to(gameScene)
-	
 func _on_QuitGame_Butoon_pressed():
 	get_tree().quit()
+
+func _ready():
+	gameScene = preload("res://scenes/World.tscn")
+
+func _on_NewGame_Button_button_down():
+	get_tree().change_scene_to(gameScene)
